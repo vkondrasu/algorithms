a067d9fa70eb0dbafcc0519a7177a8bb779aefe2
venkat kondrasu <venkat.kondrasu@gmail.com>
2018-05-04 19:44:37 +0530

    Create QuickUnionUF.java

---
+++ QuickUnionUF.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vk185107
+ */
+public class QuickUnionUF {
+    
+    private int[] id;
+    private int count;
+    
+    public QuickUnionUF(int N){
+        count = N;
+        id = new int[N];
+        
+        for(int i =0; i<N; i++){
+            id[i] = i;
+        }
+    }
+    
+    public int count(){
+        return count;
+    }
+    
+    public int find(int p){
+        while(id[p] != p){
+            p = id[p];
+        }
+        
+        return p;
+    }
+    
+    public void union(int p, int q){
+        int pRoot = find(p);
+        int qRoot = find(q);
+        
+        if(pRoot != qRoot){
+            id[pRoot] = qRoot;
+            count--;
+        }
+    }
+    
+    public boolean connected(int p, int q){
+        return find(p) == find(q);
+    }
+    
+    public static void main(String[] args){
+        int N= StdIn.readInt();
+        
+        QuickUnionUF uf = new QuickUnionUF(N);
+        
+        while(!StdIn.isEmpty()){
+            
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            
+            if(!uf.connected(p,q)){
+                uf.union(p, q);
+                
+                StdOut.println(p + " " + q);
+            }
+        }
+        
+        StdOut.println(uf.count() + "Components");
+    }
+}
